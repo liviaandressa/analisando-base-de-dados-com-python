ab60caf188f949174e45999c6dad29cae09e9621
Lívia Santos <liviaandressa61@gmail.com>
2022-02-09 09:53:07 -0300

    listando arquivos de um diretório

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# Percorrer todos os arquivos da pasta base de dados (pasta Vendas)
+# método 'listdir' lista um diretório - lista todos o arquivos que tem dentro do dietótio 
+
+
+import os
+
+lista_arquivos = os.listdir('C:\\Users\\Lívia\Desktop\\curso-python-hashtag\\Vendas')
+print(lista_arquivos)
