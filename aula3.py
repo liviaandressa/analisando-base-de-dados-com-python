8e947a51bb4ee519013c08b9f78eacd50123e45d
Lívia Santos <liviaandressa61@gmail.com>
2022-02-09 10:16:53 -0300

    importando as bases de dados de vendas

---
+++ aula3.py
@@ -0,0 +1,8 @@
+# importar as bases de dados de vendas 
+
+import os
+
+lista_arquivos = os.listdir("C:\\Users\\Lívia\Desktop\\curso-python-hashtag\\Vendas")
+
+for arquivo in lista_arquivos:
+    print(f"C:\\Users\\Lívia\Desktop\\curso-python-hashtag\\Vendas\\{arquivo}")
